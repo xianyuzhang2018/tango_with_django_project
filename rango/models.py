c3d85e75fd2da778a1c73fed8ef6b205fe747cdc
2237786z <2237786z@student.gla.ac.uk>
2019-02-10 21:10:33 +0000

    chapter 5 + exercise

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,30 @@
+from django.contrib import admin
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+    def __str__(self):
+        return self.list_display
